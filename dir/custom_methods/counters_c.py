0535f4bebc9bf6e4c9651a3f71ddbec33c5fcd6e
Bohdan Branytskyi <41265956+LaCost777@users.noreply.github.com>
2022-11-06 22:18:08 +0200

    Delete counters_c.py

--- dir/custom_methods/counters_c.py
+++
@@ -1,8 +0,0 @@
-"""This is file to keep custom counters and weird counters to learn 
-how to use them in different project as a custom libraries. """
-
-
-class BasicCode:
-    def __int__(self, some_variable):
-        self.variable = some_variable
-        return self.variable
