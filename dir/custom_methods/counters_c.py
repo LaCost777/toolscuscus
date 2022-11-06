7611ad81aa17f05c8ed41c73e2f07de760dd3d16
Bohdan Branytskyi <41265956+LaCost777@users.noreply.github.com>
2022-11-06 21:17:07 +0200

    Create some Base Class to test imports.

--- dir/custom_methods/counters_c.py
+++ dir/custom_methods/counters_c.py
@@ -1,3 +1,8 @@
 """This is file to keep custom counters and weird counters to learn 
 how to use them in different project as a custom libraries. """
 
+
+class BasicCode:
+    def __int__(self, some_variable):
+        self.variable = some_variable
+        return self.variable
